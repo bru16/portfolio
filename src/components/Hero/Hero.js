37afe6720999d47866e61af4cabeb4f18faae250
Bruno <bru_16@hotmail.com.ar>
2022-01-10 15:01:00 -0300

    projects section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,20 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there! <br />
+        I'am Bruno Aburto
+      </SectionTitle>
+      <SectionText>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy
+        text ever since the 1500s, when an unknown
+      </SectionText>
+      <Button>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
