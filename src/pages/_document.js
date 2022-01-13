b39855194701a220433cee10e4134072aa4ff4a3
Bruno <bru_16@hotmail.com.ar>
2022-01-13 19:53:49 -0300

    added .jpg ext to meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <meta name="image" property="og:image" content="images/image" />
+          <meta name="image" property="og:image" content="images/image.jpg" />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
