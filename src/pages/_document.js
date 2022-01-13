46b97a6adb36bb374d2ae23eddab22cebc0bdccf
Bruno <bru_16@hotmail.com.ar>
2022-01-13 19:52:06 -0300

    image meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <meta name="image" property="og:image" content="images/image" />
+          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
